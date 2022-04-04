7cf8150839579a1eb8399ffad247f6cad398de87
Steven12312 <steventchanra@hotmail.com>
2022-04-04 19:31:15 +0200

    delete

--- sw.js
+++ sw.js
@@ -1,25 +1,10 @@
-// const staticDevCoffee = "dev-coffee-site-v1";
+ /*const staticDevCoffee = "dev-coffee-site-v1";
 
-// self.addEventListener("install", installEvent => {
-//   console.log(installEvent);
-//   installEvent.waitUntil(
-//     caches
-//       .open(staticDevCoffee)
-//       .then(cache => {
-//         cache.addAll(assets);
-//       })
-//       .catch(console.log)
-//   );
-// });
+ self.addEventListener("install", installEvent => {
+   console.log(installEvent);
+   installEvent.waitUntil(
+     caches.open(staticDevCoffee).then(cache => {cache.addAll(assets);}).catch(console.log) );});
 
-// self.addEventListener("fetch", event => {
-//   // console.log(event.request);
-//   event.respondWith(
-//     caches
-//       .match(event.request)
-//       .then(res => {
-//         return res || fetch(event.request);
-//       })
-//       .catch(console.log)
-//   );
-// });
+self.addEventListener("fetch", event => { console.log(event.request);
+event.respondWith(
+caches.match(event.request).then(res => { return res || fetch(event.request);}).catch(console.log) ); }); */
